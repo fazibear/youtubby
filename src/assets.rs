d1d9304427ec0d6e2cdaf1babee400038d07baf7
Michał Kalbarczyk <fazibear@gmail.com>
2024-05-08 16:07:42 +0200

    cleanup code

---
+++ src/assets.rs
@@ -0,0 +1,25 @@
+use image::{DynamicImage, ImageBuffer, Rgba};
+
+pub const INIT_SCRIPT: &str =
+    concat!(
+    "window.addEventListener('load', () => {document.head.insertAdjacentHTML('beforeend',`<style>",
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/stylesheet.css")),
+    "</style>`);});"
+);
+
+pub const LOGO: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/youtubby.png"));
+pub const ICON: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/icon.png"));
+
+pub fn get_image(data: &[u8]) -> (Vec<u8>, u32, u32) {
+    let image = image::load_from_memory(data).unwrap().into_rgba8();
+    let width = image.dimensions().0;
+    let height = image.dimensions().1;
+    (image.into_raw(), width, height)
+}
+
+// static ICON_IMAGE: DynamicImage = image::load_from_memory(ICON_DATA).unwrap();
+//
+// pub static ICON_RGBA: ImageBuffer<Rgba<u8>, Vec<u8>> = ICON_IMAGE.into_rgba8();
+// pub const ICON: Vec<u8> = ICON_RGBA.into_raw();
+// pub const ICON_WIDTH: u32 = ICON_RGBA.dimensions().0;
+// pub const ICON_HEIGHT: u32 = ICON_RGBA.dimensions().1;
--- src/window_handler.rs
+++ src/window_handler.rs
@@ -7,6 +7,8 @@ use tao::{
 };
 use wry::{WebView, WebViewBuilder};
 
+use crate::assets;
+
 pub struct WindowHandler {
     pub window: Window,
     pub webview: WebView,
@@ -14,12 +16,15 @@ pub struct WindowHandler {
 
 impl WindowHandler {
     pub fn new(event_loop: &EventLoop<()>) -> WindowHandler {
+        let (icon, icon_width, icon_height) = assets::get_image(assets::ICON);
         let window = WindowBuilder::new()
             .with_inner_size(PhysicalSize::new(1024, 2048))
             .with_titlebar_transparent(true)
             .with_fullsize_content_view(true)
             .with_title_hidden(true)
-            .with_window_icon(Some(Self::load_icon()))
+            .with_window_icon(Some(
+                Icon::from_rgba(icon, icon_width, icon_height).unwrap(),
+            ))
             .build(event_loop)
             .unwrap();
 
@@ -48,46 +53,7 @@ impl WindowHandler {
         .with_user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
         .with_url("https://music.youtube.com")
         .with_devtools(true)
-        .with_initialization_script("
-          window.addEventListener('load', () => {
-            console.log('tadam');
-            document.head.insertAdjacentHTML(
-              'beforeend',
-              `<style>
-                 html {
-                    overflow: hidden !important;
-                    height: 100% !important;
-                 }
-                 body {
-                   overscroll-behavior: none !important;
-                   height: 100% !important;
-                   overflow: auto !important;
-                 }
-                 ytmusic-nav-bar {
-                   margin-top: 15px;
-                    }
-                #guide-content {
-                    padding-top: 15px;
-                    }
-              </style>`
-            );
-          });
-        ")
-        .with_drag_drop_handler(|e| {
-            match e {
-                wry::DragDropEvent::Enter { paths, position } => {
-                    println!("DragEnter: {position:?} {paths:?} ")
-                }
-                wry::DragDropEvent::Over { position } => println!("DragOver: {position:?} "),
-                wry::DragDropEvent::Drop { paths, position } => {
-                    println!("DragDrop: {position:?} {paths:?} ")
-                }
-                wry::DragDropEvent::Leave => println!("DragLeave"),
-                _ => {}
-            }
-
-            true
-        })
+        .with_initialization_script(assets::INIT_SCRIPT)
         .build()
         .unwrap();
 
@@ -103,17 +69,5 @@ impl WindowHandler {
         {
             *control_flow = ControlFlow::Exit
         }
-    }
-    fn load_icon() -> Icon {
-        let path = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/icon.png");
-        let (icon_rgba, icon_width, icon_height) = {
-            let image = image::open(path)
-                .expect("Failed to open icon path")
-                .into_rgba8();
-            let (width, height) = image.dimensions();
-            let rgba = image.into_raw();
-            (rgba, width, height)
-        };
-        Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
     }
 }
