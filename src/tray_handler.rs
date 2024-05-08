c8b6a5859453057a7bddf677c0c578fd2e12dc78
Michał Kalbarczyk <fazibear@gmail.com>
2024-05-09 00:29:07 +0200

    update icons

--- src/tray_handler.rs
+++ src/tray_handler.rs
@@ -1,21 +1,24 @@
-use muda::MenuEvent;
+use muda::MenuId;
+use tao::event_loop::ControlFlow;
 use tray_icon::{
-    menu::{AboutMetadata, Menu, MenuItem, PredefinedMenuItem},
-    Icon, TrayIcon, TrayIconBuilder,
+    menu::{AboutMetadata, Menu, MenuEvent, MenuEventReceiver, MenuItem, PredefinedMenuItem},
+    Icon, TrayIcon, TrayIconBuilder, TrayIconEvent, TrayIconEventReceiver,
 };
 
 use crate::assets;
 
 pub struct TrayHandler {
-    menu: Menu,
     icon: TrayIcon,
+    menu_channel: &'static MenuEventReceiver,
+    tray_channel: &'static TrayIconEventReceiver,
 }
 
 impl TrayHandler {
     pub fn new() -> TrayHandler {
         let menu = Menu::new();
 
-        let quit_i = MenuItem::new("Quit", true, None);
+        let quit_i = MenuItem::with_id(MenuId::new("quit"), "Quit", true, None);
+
         let (icon_data, icon_width, icon_height) = assets::get_image(assets::LOGO);
         let icon = tray_icon::menu::Icon::from_rgba(icon_data, icon_width, icon_height).unwrap();
 
@@ -47,9 +50,25 @@ impl TrayHandler {
             .build()
             .unwrap();
 
-        let channel = MenuEvent::receiver();
-        TrayHandler { menu, icon }
+        let menu_channel = MenuEvent::receiver();
+        let tray_channel = TrayIconEvent::receiver();
+
+        TrayHandler {
+            menu_channel,
+            tray_channel,
+            icon,
+        }
     }
 
-    pub fn try_recv(&self) {}
+    pub fn try_recv(&self, control_flow: &mut ControlFlow) {
+        if let Ok(event) = self.menu_channel.try_recv() {
+            if event.id == "quit" {
+                *control_flow = ControlFlow::Exit;
+            }
+        }
+
+        if let Ok(event) = self.tray_channel.try_recv() {
+            //todo!()
+        }
+    }
 }
