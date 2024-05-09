98a2337974f2aa5d90c7fb938271ebd8b9bfb0e9
Michał Kalbarczyk <fazibear@gmail.com>
2024-05-09 22:48:44 +0200

    Adding macos build script

--- src/assets.rs
+++ src/assets.rs
@@ -1,9 +1,9 @@
 pub const INIT_SCRIPT: &str =
     concat!(
-    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/scripts.js")),
     "window.addEventListener('load', () => {document.head.insertAdjacentHTML('beforeend',`<style>",
     include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/stylesheet.css")),
-    "</style>`);});"
+    "</style>`);});",
+    include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/scripts.js"))
 );
 
 pub const LOGO: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/youtubby.png"));
@@ -15,10 +15,3 @@ pub fn get_image(data: &[u8]) -> (Vec<u8>, u32, u32) {
     let height = image.dimensions().1;
     (image.into_raw(), width, height)
 }
-
-// static ICON_IMAGE: DynamicImage = image::load_from_memory(ICON_DATA).unwrap();
-//
-// pub static ICON_RGBA: ImageBuffer<Rgba<u8>, Vec<u8>> = ICON_IMAGE.into_rgba8();
-// pub const ICON: Vec<u8> = ICON_RGBA.into_raw();
-// pub const ICON_WIDTH: u32 = ICON_RGBA.dimensions().0;
-// pub const ICON_HEIGHT: u32 = ICON_RGBA.dimensions().1;
