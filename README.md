3734f6c4d8be1149fe88c25e6e3edea6fefa7a35
Michał Kalbarczyk <fazibear@gmail.com>
2024-05-13 23:54:59 +0200

    update readmefile

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# YouTubby
+# Youtubby
+<img width="320" height="320" src="https://github.com/fazibear/youtubby/blob/master/assets/youtubby.png?raw=true" align="right"/>
 
-Desktop Youtube Music Player
+> For a long time I was looking for desktop Youtube Music Player. I write my own. Yep in Rust BTW ;)
+
+Goal is to supports all platforms, but now focus is on MacOS.
--- src/menu_handler.rs
+++ src/menu_handler.rs
@@ -13,13 +13,14 @@ impl MenuHandler {
     pub fn new() -> Self {
         let menu = Menu::new();
 
-        let quit_i = MenuItem::with_id(MenuId::new("quit"), "Quit", true, None);
+        let quit_i = MenuItem::with_id(MenuId::new("quit"), "Quit Youtubby", true, None);
 
         let (icon_data, icon_width, icon_height) = assets::get_image(assets::LOGO);
         let icon = tray_icon::menu::Icon::from_rgba(icon_data, icon_width, icon_height).unwrap();
 
         menu.append_items(&[
             &CheckMenuItem::with_id("check-custom-1", "Check Custom 1", true, true, None),
+            &PredefinedMenuItem::separator(),
             &PredefinedMenuItem::about(
                 None,
                 Some(AboutMetadata {
@@ -31,7 +32,6 @@ impl MenuHandler {
                     ..Default::default()
                 }),
             ),
-            &PredefinedMenuItem::separator(),
             &quit_i,
         ])
         .unwrap();
