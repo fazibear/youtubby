1ef05f5ce2e77b5c5ddb4cef5eba3fbe4e810a93
Michał Kalbarczyk <m.kalbarczy2@samsung.com>
2024-05-15 13:41:52 +0200

    add debugs

--- src/tray_handler.rs
+++ src/tray_handler.rs
@@ -14,7 +14,7 @@ impl TrayHandler {
         let icon = TrayIconBuilder::new()
             .with_id("0")
             .with_menu(Box::new(menu_handler.menu.clone()))
-            .with_menu_on_left_click(false)
+            //.with_menu_on_left_click(false)
             .with_icon(icon_data)
             .build()
             .unwrap();
