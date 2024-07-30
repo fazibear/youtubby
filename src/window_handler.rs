2ae493eb684e3265e0753f4a7670a3586a8d4cd6
Michał Kalbarczyk <fazibear@gmail.com>
2024-07-30 15:13:24 +0200

    rename to platform

--- src/window_handler.rs
+++ src/window_handler.rs
@@ -1,19 +1,4 @@
-#[cfg(target_os = "windows")]
-mod windows;
-#[cfg(target_os = "windows")]
-pub use windows::platform;
-
-#[cfg(target_os = "linux")]
-mod linux;
-#[cfg(target_os = "linux")]
-pub use linux::platform;
-
-#[cfg(target_os = "macos")]
-mod macos;
-#[cfg(target_os = "macos")]
-pub use macos::platform;
-
-use crate::{player_state_changed::PlayerStateChanged, Youtubby};
+use crate::{platform::platform, player_state_changed::PlayerStateChanged, Youtubby};
 use anyhow::Result;
 use tao::{
     dpi::{LogicalSize, PhysicalPosition, PhysicalSize},
